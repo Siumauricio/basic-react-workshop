0358beb757adff07e88f4f9afc1aca891633f6c3
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2023-02-28 01:01:11 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+};
